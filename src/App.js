1ed0027313f9e8dba3d19442802e77b159f64e7f
Alexandru Morarasu <c_amorarasu@groupon.com>
2019-02-28 16:27:46 +0200

    initialize

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {observer} from "mobx-react";
+import { action } from "mobx";
 
+@observer(['TodoListStore', 'Counter'])
 class App extends Component {
+  constructor() {
+    super();
+    console.log('this.props.todolistStore');
+  }
+
+  // @observable counter = 0;
+  @action.bound
+  increment() {
+    this.props.Counter.counter++;
+  }
+  @action.bound
+  decrement() {
+    this.props.Counter.counter--;
+  }
   render() {
+
+    const {todoList} = this.props.TodoListStore;
+    const {counter} = this.props.Counter;
+    console.log('counter: ', counter);
+
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <ul>
+          {todoList.map((entry, i) => <li key={i}>{entry.msg}</li>)}
+        </ul>
+        <div>
+          <p>{counter}</p>
+          <button onClick={this.increment}>increment</button>
+          <button onClick={this.decrement}>decremenet</button>
+        </div>
       </div>
     );
   }
